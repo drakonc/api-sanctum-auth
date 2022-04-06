e76fbd11685135c60307142b698546a03b7f7a64
Jose Castro <drakonc@gmail.com>
2022-04-06 11:57:35 -0500

    ADD: Añadiendo las rutas y las funciones vacias en el controller

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,8 +5,23 @@ namespace App\Http\Controllers\Api;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    
+    public function register(Request $request){
+
+    }
+
+    public function login(Request $request){
+        
+    }
+
+    public function userProfile(){
+        
+    }
+
+    public function logout(){
+        
+    }
 }
